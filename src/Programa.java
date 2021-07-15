ea85fa3abb4d3856d3f04e8f4eb4c0ad56811ee2
sidneyrdm <sidneybritomal@gmail.com>
2021-07-15 15:49:43 -0300

    first commit

---
+++ src/Programa.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Digite o valor de A.: ");
+        int a = scanner.nextInt();
+
+        System.out.println("Digite o valor de B.: ");
+        int b = scanner.nextInt();
+
+
+        System.out.println("Soma de A + B = "+(a+b));
+
+    }
+
+}
