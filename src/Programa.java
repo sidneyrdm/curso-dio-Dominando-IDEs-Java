864c201e81133b226dccd35811caf62287bf7174
sidneyrdm <sidneybritomal@gmail.com>
2021-07-15 16:14:45 -0300

    criação da classe calculadora

--- src/Programa.java
+++ src/Programa.java
@@ -1,48 +1,31 @@
+import calculadora.Calculadora;
+
 import java.util.Scanner;
 
 public class Programa {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         Livro livro = new Livro("Em busca do Sentido da vida", 250);
 
         System.out.println(livro);
 
+        System.out.println("Digite o valor de A.: ");
+        int a = scanner.nextInt();
+
+        System.out.println("Digite o valor de B.: ");
+        int b = scanner.nextInt();
+
+        Calculadora calc = new Calculadora();
+
+        System.out.println("Soma.: "+calc.somar(a,b));
+        System.out.println("Subtração.: "+calc.subtrair(a,b));
+        System.out.println("Multiplicação.: "+calc.multiplicar(a,b));
+        System.out.println("Divisão.: "+calc.dividir(a,b));
+
+
     }
 
 }
 
-class Livro{
-
-    private String nome;
-    private Integer numeroPaginas;
-
-    public Livro(String nome, Integer numeroPaginas) {
-        this.nome = nome;
-        this.numeroPaginas = numeroPaginas;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Integer getNumeroPaginas() {
-        return numeroPaginas;
-    }
-
-    public void setNumeroPaginas(Integer numeroPaginas) {
-        this.numeroPaginas = numeroPaginas;
-    }
-
-    @Override
-    public String toString() {
-        return "Livro{" +
-                "nome='" + nome + '\'' +
-                ", numeroPaginas=" + numeroPaginas +
-                '}';
-    }
-}
