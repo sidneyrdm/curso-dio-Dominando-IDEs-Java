3321c1e684ce5f4cd47b9b149ce6978292a19c5e
sidneyrdm <sidneybritomal@gmail.com>
2021-07-15 15:55:53 -0300

    criação da classe livro

--- src/Programa.java
+++ src/Programa.java
@@ -3,18 +3,46 @@ import java.util.Scanner;
 public class Programa {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Livro livro = new Livro("Em busca do Sentido da vida", 250);
 
-        System.out.println("Digite o valor de A.: ");
-        int a = scanner.nextInt();
-
-        System.out.println("Digite o valor de B.: ");
-        int b = scanner.nextInt();
-
-
-        System.out.println("Soma de A + B = "+(a+b));
+        System.out.println(livro);
 
     }
 
+}
+
+class Livro{
+
+    private String nome;
+    private Integer numeroPaginas;
+
+    public Livro(String nome, Integer numeroPaginas) {
+        this.nome = nome;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(Integer numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numeroPaginas=" + numeroPaginas +
+                '}';
+    }
 }
